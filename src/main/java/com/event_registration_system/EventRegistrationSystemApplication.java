e97eb52b2c3cc59397840e242e5132abc6337db7
trivedi089 <satyachbs007@gmail.com>
2025-09-04 22:48:51 +0530

    initial-setup

---
+++ src/main/java/com/event_registration_system/EventRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.event_registration_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/event_registration_system/EventRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event_registration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
