1a2f2d36daf69fb69c30f838d7e703ff9e71b5d1
trivedi089 <satyachbs007@gmail.com>
2025-09-05 23:16:41 +0530

    user-entity-and-repo-spring-security

---
+++ src/main/java/com/event_registration_system/entities/User.java
@@ -0,0 +1,34 @@
+package com.event_registration_system.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @Email(message = "Email should be valid")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles = new HashSet<>();
+}
---
+++ src/main/java/com/event_registration_system/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.event_registration_system.repository;
+
+import com.event_registration_system.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
